195a95a6e1fb403bbae61300df7828d97f86d4b5
HuirouMa <mahuirou456@gmail.com>
2023-04-23 21:52:14 -0500

    text align

--- game-app/src/components/GameCard.tsx
+++ game-app/src/components/GameCard.tsx
@@ -22,7 +22,7 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <HStack>
-          <Heading fontSize="2xl" textAlign="left">
+          <Heading fontSize="2xl">
             {game.name}
             <Emoji rating={game.rating_top} />
           </Heading>
